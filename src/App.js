a84c395bb82ac93546595bd1ddb77d2779f416c4
kemo12 <dodkamel919@gmail.com>
2021-05-08 21:00:03 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import {ChatEngine} from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed.jsx';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './app.css';
 
+const projectID = '87cde2ce-eea2-41c0-8697-825edab0d0ad';
+
 const App = () => {
-    return (
-        <ChatEngine
-            height="100vh"
-            projectID="87cde2ce-eea2-41c0-8697-825edab0d0ad"
-            userName="admin"
-            userSecret="123456"
-            renderChatFeed={(ChatAppProps)=><ChatFeed {...ChatAppProps}/>}
-        
-        />
-    )
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
 export default App;
