539bf2856e42038390bf011db2d7029f7ebbf7a1
kemo12 <dodkamel919@gmail.com>
2021-05-08 04:08:16 +0300

    seconed commit

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import {ChatEngine} from 'react-chat-engine';
-import './App.css';
+import ChatFeed from './components/ChatFeed.jsx';
+import './app.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="87cde2ce-eea2-41c0-8697-825edab0d0ad"
+            userName="admin"
+            userSecret="123456"
+            renderChatFeed={(ChatAppProps)=><ChatFeed {...ChatAppProps}/>}
+        
+        />
+    )
+}
+
+export default App;
